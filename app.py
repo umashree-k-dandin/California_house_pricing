1075eb6a129c1de9219c7cdfd8cb11d76184eb3d
Umashree Dandin <umadandin84@gmail.com>
2026-09-15 11:58:48 +0530

    The Web App Is working is fine

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('regmodel.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb')) 
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict_api',methods = ['POST'])
+
+def predict_api():
+    data = request.get_json(force=True)
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+@app.route('/predict',methods=['Post'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House Price Prediction value is {}".format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # Further processing of the data and prediction would go here
